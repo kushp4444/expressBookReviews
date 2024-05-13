a2f33d76ec6897fa639457f80ae226dc537c42ec
kushp4444 <150421866+kushp4444@users.noreply.github.com>
2024-05-13 13:01:53 -0400

    Implement asynchronous functions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,15 +25,36 @@ public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4))
 });
 
+public_users.get('/async-get-books', async function (req, res) {
+  const getBooks = new Promise((resolve, reject) => {
+    resolve(JSON.stringify(books,null,4));
+  });
+  getBooks.then((bookList) => res.send(bookList));
+
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = parseInt(req.params.isbn)
   if (!books[isbn]) {
-    return res.send("No book with that ISBN exists in our library")
+    return res.status(404).send("No book with that ISBN exists in our library")
   } else {
     res.send(books[isbn])
   }
  });
+
+public_users.get('/async-isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = parseInt(req.params.isbn);
+    if (!books[isbn]) {
+      return res.status(404).send("No book with that ISBN exists in our library");
+    }
+    res.send(books[isbn]);
+  } catch (error) {
+    res.status(500).send("An error occurred while retrieving the book");
+  }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -48,6 +69,22 @@ public_users.get('/author/:author',function (req, res) {
   res.send(JSON.stringify(booksByAuthor,null,4))
 });
 
+public_users.get('/async-author/:author', async function (req, res) {
+  try {
+    const author = req.params.author
+    let booksByAuthor = []
+    for (let [isbn, details] of Object.entries(books)) {
+      if (details.author === author) {
+        booksByAuthor.push(isbn);
+        booksByAuthor.push(details);
+      }
+    }
+    res.send(JSON.stringify(booksByAuthor,null,4))
+  } catch (error) {
+    res.status(500).send("An error occurred while retrieving the book");
+  }
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title
@@ -59,6 +96,22 @@ public_users.get('/title/:title',function (req, res) {
     }
   }
   res.send(JSON.stringify(booksByTitle,null,4))
+});
+
+public_users.get('/async-title/:title', async function (req, res) {
+  try {
+    const title = req.params.title
+    let booksByTitle = []
+    for (let [isbn, details] of Object.entries(books)) {
+      if (details.title === title) {
+        booksByTitle.push(isbn);
+        booksByTitle.push(details);
+      }
+    }
+    res.send(JSON.stringify(booksByTitle,null,4))
+  } catch (error) {
+    res.status(500).send("An error occurred while retrieving the book");
+  }
 });
 
 //  Get book review
